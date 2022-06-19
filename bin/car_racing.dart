2bac055b055da354c50f453c373f215e4f35ed54
jpoh281 <hjp4869@naver.com>
2022-06-19 21:56:05 +0900

    🎉 프로젝트 생성

---
+++ bin/car_racing.dart
@@ -0,0 +1,5 @@
+import 'package:car_racing/car_racing.dart' as car_racing;
+
+void main(List<String> arguments) {
+  print('Hello world: ${car_racing.calculate()}!');
+}
---
+++ test/car_racing_test.dart
@@ -0,0 +1,8 @@
+import 'package:car_racing/car_racing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
