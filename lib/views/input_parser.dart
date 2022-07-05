b52ac963f9e3959af2e01102af0afcf95486f416
jpoh281 <hjp4869@naver.com>
2022-07-06 00:18:58 +0900

    ✅ 닉네임 파싱 테스트 추가

--- lib/views/input_parser.dart
+++ lib/views/input_parser.dart
@@ -1,20 +1,28 @@
 class InputParser {
-  List<String> parseStringToCarNames(String? inputText) {
-    List<String> parsedByComma = _parseByComma(inputText);
 
-    for (String name in parsedByComma) {
+  List<String> parseStringToCarNames(String? inputText) {
+
+    if(inputText == null || inputText.isEmpty){
+      throw FormatException('문자를 입력해 주세요.');
+    }
+
+    List<String> names = _parseByComma(inputText);
+
+    for (String name in names) {
       _checkOverFiveWords(name);
     }
 
-    return parsedByComma;
+    return names;
   }
 
-  List<String> _parseByComma(String? inputText) {
-    throw UnimplementedError();
+  List<String> _parseByComma(String inputText) {
+    return inputText.split(',');
   }
 
   void _checkOverFiveWords(String name) {
-    throw UnimplementedError();
+    if(name.length > 5){
+      throw FormatException('5자를 초과하지 않는 닉네임으로 입력해 주세요.');
+    }
   }
 
   int parseStringToRound(String? inputText) {
--- test/input_parser_test.dart
+++ test/input_parser_test.dart
@@ -4,17 +4,56 @@ import 'package:test/test.dart';
 void main() {
   late final InputParser inputParser;
 
-  setUpAll(() => {
-        setUp(() {
-          inputParser = InputParser();
-        }),
+  setUpAll(() {
+    inputParser = InputParser();
+  });
+
+  group("InputParser 클래스의 parseStringToCarNames 메소드 :", () {
+    test("오류를 통해 [null]이 입력되면 [FormatException]을 던진다.", () {
+      String? inputText;
+
+      expect(() => inputParser.parseStringToCarNames(inputText),
+          throwsFormatException);
+    });
+
+    test("유저가 [''] 을 입력하면 [FormatException]을 던진다.", () {
+      String? inputText = '';
+
+      expect(() => inputParser.parseStringToCarNames(inputText),
+          throwsFormatException);
+    });
+
+    test('5자를 초과하는 이름이 있을 경우 에러 반환', () {
+      String inputText = '1,12,123,1234,12345,123456';
+      expect(() => inputParser.parseStringToCarNames(inputText),
+          throwsFormatException);
+    });
+
+    List<dynamic> testTexts = [
+      [
+        '혼자왔어요',
+        ['혼자왔어요']
+      ],
+      [
+        '혼자왔어요,둘이왔어요,셋이왔어요',
+        ['혼자왔어요', '둘이왔어요', '셋이왔어요']
+      ],
+      [
+        '혼자왔어요,둘이왔어요,셋이왔어요,둘이왔어요,혼자왔어요',
+        ['혼자왔어요', '둘이왔어요', '셋이왔어요', '둘이왔어요', '혼자왔어요']
+      ],
+    ];
+
+    for (var testText in testTexts) {
+      test(
+          "유저가 입력한 값: \"${testText[0]}\" , 단위로 구분해서 배열로 저장한다. 결과값: ${testText[1]}]",
+          () {
+        expect(inputParser.parseStringToCarNames(testText[0]), testText[1]);
       });
+    }
+  });
 
-  group("InputParser 클래스의 parseStringToCarNames 메소드", () {
-    test("유저가 입력한 값을 , 단위로 구분해서 배열에 저장한다.", () {});
+  group("InputParser 클래스의 parseStringToRound 메소드 :", () {
 
-    test('5자를 초과하는 이름이 있을 경우 에러 반환', () {});
-
-    test('5자를 초과하는 이름이 없을 경우 성공적으로 배열을 반환한다. ', () {});
   });
 }
