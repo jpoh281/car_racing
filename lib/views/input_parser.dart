db0a724f86af335ce29cf1cd42a691e26ec8f3ed
jpoh281 <hjp4869@naver.com>
2022-07-06 00:36:25 +0900

    ✅ 라운드 파싱 기능 및 테스트 코드 추가

--- lib/views/input_parser.dart
+++ lib/views/input_parser.dart
@@ -1,12 +1,8 @@
 class InputParser {
-
   List<String> parseStringToCarNames(String? inputText) {
+    _checkEmptyOrNull(inputText);
 
-    if(inputText == null || inputText.isEmpty){
-      throw FormatException('문자를 입력해 주세요.');
-    }
-
-    List<String> names = _parseByComma(inputText);
+    List<String> names = _parseByComma(inputText!);
 
     for (String name in names) {
       _checkOverFiveWords(name);
@@ -20,12 +16,34 @@ class InputParser {
   }
 
   void _checkOverFiveWords(String name) {
-    if(name.length > 5){
-      throw FormatException('5자를 초과하지 않는 닉네임으로 입력해 주세요.');
+    if (name.length > 5) {
+      throw FormatException('5자를 초과하지 않는 닉네임으로 입력해 주세요.\n');
     }
   }
 
   int parseStringToRound(String? inputText) {
-    throw UnimplementedError();
+    _checkEmptyOrNull(inputText);
+
+    RegExp integerRules = RegExp('^-?\\d\$');
+
+    var regExpResult = integerRules.allMatches(inputText!).toList();
+
+    if (regExpResult.isEmpty) {
+      throw FormatException('숫자를 입력해 주세요.\n');
+    }
+
+    var integer = int.parse(regExpResult[0].group(0)!);
+
+    if (integer < 0) {
+      throw FormatException('0 보다 큰 숫자를 입력해주세요.\n');
+    }
+
+    return integer;
+  }
+
+  _checkEmptyOrNull(String? inputText) {
+    if (inputText == null || inputText.isEmpty) {
+      throw FormatException('문자를 입력해 주세요.\n');
+    }
   }
 }
--- test/input_parser_test.dart
+++ test/input_parser_test.dart
@@ -23,7 +23,7 @@ void main() {
           throwsFormatException);
     });
 
-    test('5자를 초과하는 이름이 있을 경우 에러 반환', () {
+    test('5자를 초과하는 이름을 입력하면 [FormatException]을 던진다.', () {
       String inputText = '1,12,123,1234,12345,123456';
       expect(() => inputParser.parseStringToCarNames(inputText),
           throwsFormatException);
@@ -46,7 +46,7 @@ void main() {
 
     for (var testText in testTexts) {
       test(
-          "유저가 입력한 값: \"${testText[0]}\" , 단위로 구분해서 배열로 저장한다. 결과값: ${testText[1]}]",
+          "유저가 입력한 값: \"${testText[0]}\" , 단위로 구분해서 배열에 저장한 뒤 반환한다. 결과값: ${testText[1]}]",
           () {
         expect(inputParser.parseStringToCarNames(testText[0]), testText[1]);
       });
@@ -54,6 +54,31 @@ void main() {
   });
 
   group("InputParser 클래스의 parseStringToRound 메소드 :", () {
+    test("오류를 통해 [null]이 입력되면 [FormatException]을 던진다.", () {
+      String? inputText;
 
+      expect(() => inputParser.parseStringToRound(inputText),
+          throwsFormatException);
+    });
+
+    test("유저가 [''] 을 입력하면 [FormatException]을 던진다.", () {
+      String? inputText = '';
+
+      expect(() => inputParser.parseStringToRound(inputText),
+          throwsFormatException);
+    });
+
+    test("유저가 음수를 입력하면 [FormatException]을 던진다.", () {
+      String? inputText = '-1';
+
+      expect(() => inputParser.parseStringToRound(inputText),
+          throwsFormatException);
+    });
+
+    test("유저가 양수를 입력하면 [int]를 반환한다.", () {
+      String? inputText = '1';
+
+      expect(inputParser.parseStringToRound(inputText), 1);
+    });
   });
 }
